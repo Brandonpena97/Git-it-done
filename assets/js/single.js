18378e8675c13a5d86d1520c8f41a7fee750a784
Brandon Pena <brandonpena@gmail.com>
2021-11-02 14:21:44 -0400

    add extra message

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,19 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var displayWarning = function (repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
 
 var getRepoIssues = function (repo) {
     console.log(repo);
@@ -10,7 +25,13 @@ var getRepoIssues = function (repo) {
         if (response.ok) {
             response.json().then(function (data) {
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
+
         }
         else {
             alert("There was a problem with your request!");
@@ -24,7 +45,7 @@ var displayIssues = function (issues) {
     if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues!";
         return;
-      }
+    }
 
 
     for (var i = 0; i < issues.length; i++) {
@@ -58,6 +79,7 @@ var displayIssues = function (issues) {
     }
 
 };
+
 
 
 getRepoIssues("facebook/react");
